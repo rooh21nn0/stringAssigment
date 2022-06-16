445db1001c4c3e098250bfe9736bb6183579a18d
Roohullah <roohullahali78@gmail.com>
2022-06-16 20:03:33 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// words suggestion app
+
+// 1. create a empty loop with the name of lattersArray;
+// 2. create a input, get user value and store in a variable;
+// 3. create an Alphabates array wihich is holding Alphabates;
+// 4. create a function call RandomWords(), it can generate random latter using availble Alphabates, rondmise latters can make a word which is find by user;
+// 5. words can hold 3 to 6 latters
+// 6. user have 3 life to find the word
+// 7. if user is inout the correct word it will win othervise gameOver after 3 attempts.
+
+
+
+var latterArray = [];
+var alphabates = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var number = math.random();
+
+
+
+
+
+
+
+
+// for(var i=0; i<alphabates.length; i++){
+
+// }
